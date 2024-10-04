80c00385529015ad7ac17f5485d201acf98942e8
Ernest Lin <linchongtik@gmail.com>
2024-10-03 21:23:47 -0700

    Set up basic project structure and installed daisyUI for styling.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from 'daisyui'
 
 const config: Config = {
   content: [
@@ -8,12 +9,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui:{
+
+  }
 };
 export default config;
