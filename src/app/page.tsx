7067ae1b106b8fc22cfcaa32b2b581c1e070bb44
Ernest Lin <linchongtik@gmail.com>
2024-10-05 18:23:18 -0700

    refactor the code from Home page to GroceryItemList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,5 @@
-"use client";
-
-import { useState } from "react";
-import Form from "./components/Form";
-import DisplayTable from "./components/DisplayTable";
-import Item from "./types/Item";
-import { sampleData } from "../data/sampleData";
+import GroceryItemList from "./components/GroceryItemList";
 
 export default function Home() {
-  const [data, setData] = useState<Item[]>(sampleData);
-
-  const handleAddItem = (item: Item) => {
-    setData([...data, item]);
-  };
-
-  return (
-    <div className="m-3 flex flex-col gap-y-3">
-      <h1>Grocery Itme Record list</h1>
-      <Form onAddItem={handleAddItem} />
-      <DisplayTable data={data} />
-    </div>
-  );
+  return <GroceryItemList />;
 }
